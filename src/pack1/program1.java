7e7215ef99a23c5fb8f234ece646cf07314bf4e7
ashitha123456 <ashithashetty064@gmail.com>
2019-04-24 12:54:38 +0530

    second commit

--- src/pack1/program1.java
+++ src/pack1/program1.java
@@ -4,6 +4,7 @@ public class program1 {
 
 	public static void main(String[] args) {
 		System.out.println("java");
+		System.out.println("test");
 
 	}
 
---
+++ src/pack1/program2.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		System.out.println("hii");
+
+	}
+
+}
